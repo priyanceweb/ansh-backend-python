b0fc81ccf9aee58ac2fb7ac6233a199ecf055b7e
priyance mangla <priyanshmangla@gmail.com>
2024-11-08 13:52:15 +0530

    do

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ ocr = PaddleOCR(use_angle_cls=True, lang='en') # need to run only once to downlo
 # on the terminal type: curl http://127.0.0.1:5000/ 
 # returns hello world when we use GET. 
 # returns the data that we send when we use POST. 
+
+@app.route('/', methods = ['GET']) :
+    return "Ok"
+
 @app.route('/upload-image', methods = ['POST']) 
 def home(): 
 
