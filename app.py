096b442ccc96f1d14e314b5603d0f48bbdf39ce5
priyance mangla <priyanshmangla@gmail.com>
2024-11-08 13:57:49 +0530

    do

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ ocr = PaddleOCR(use_angle_cls=True, lang='en') # need to run only once to downlo
 # returns the data that we send when we use POST. 
 
 @app.route('/', methods = ['GET'])
+def health_check():
     return "Ok"
 
 @app.route('/upload-image', methods = ['POST']) 
